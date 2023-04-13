023340585604721952eeee3a20935c46452b2b4e
Gazdag Balázs <gazdag.balazs8.a@gmail.com>
2023-04-13 17:43:05 +0200

    Javascript Objektum létrehozva

---
+++ project01/app.js
@@ -0,0 +1,45 @@
+//OBjektum létrehozása 
+
+const orders = [
+    {
+        id:1,
+        pizza: "Hawaii",
+        extra: "Kukorica",
+        ital: "Pepsi Zero",
+        ar: 3010, 
+        kep: "https://www.pizzaboypecs.hu/upload/images/products/IMG_0090k.png"
+        },
+    {
+        id:2,
+        pizza: "Margaréta",
+        extra: "Oliva",
+        ital: "Coca Cola Zero",
+        ar: 2910, 
+        kep: "https://www.pizzaboypecs.hu/upload/images/products/IMG_0007_k.png"
+        },
+    {
+        id:3,
+        pizza: "Calzone",
+        extra: "Füstölt Sajt",
+        ital: "Sprite",
+        ar: 3900, 
+        kep: "https://www.pizzaboypecs.hu/upload/images/products/IMG_0162k.png"
+        },
+    {
+        id:4,
+        pizza: "Son-Go-Ku",
+        extra: "Bacon",
+        ital: "Fanta",
+        ar: 3180, 
+        kep: "https://www.pizzaboypecs.hu/upload/images/products/IMG_0315k.png"
+        },
+    {
+        id:5,
+        pizza: "All-in",
+        extra: "Kukorica",
+        ital: "Narancslé",
+        ar: 4230, 
+        kep: "https://www.pizzaboypecs.hu/upload/images/products/IMG_0892k.png"
+        },
+        
+]
